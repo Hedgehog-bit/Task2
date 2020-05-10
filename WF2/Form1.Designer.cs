dbb4e5f35e6ab2be057f4e5d671c9acfc30ea265
Hedgehog-bit <65031811+Hedgehog-bit@users.noreply.github.com>
2020-05-10 18:31:14 +0300

    Add files via upload

---
+++ WF2/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WF2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.x = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Output1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.y = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Priz = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Output2 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(657, 384);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.y);
+            this.tabPage1.Controls.Add(this.Output1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.x);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(649, 358);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Задание2.1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.Output2);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.Priz);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(649, 358);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Задание2.2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // x
+            // 
+            this.x.Location = new System.Drawing.Point(402, 72);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(100, 20);
+            this.x.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите x";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(246, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Определить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Output1
+            // 
+            this.Output1.Location = new System.Drawing.Point(268, 271);
+            this.Output1.Name = "Output1";
+            this.Output1.Size = new System.Drawing.Size(100, 20);
+            this.Output1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введите y";
+            // 
+            // y
+            // 
+            this.y.Location = new System.Drawing.Point(402, 105);
+            this.y.Name = "y";
+            this.y.Size = new System.Drawing.Size(100, 20);
+            this.y.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Введите признак:";
+            // 
+            // Priz
+            // 
+            this.Priz.Location = new System.Drawing.Point(405, 80);
+            this.Priz.Name = "Priz";
+            this.Priz.Size = new System.Drawing.Size(100, 20);
+            this.Priz.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(260, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Определить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Output2
+            // 
+            this.Output2.Location = new System.Drawing.Point(178, 262);
+            this.Output2.Name = "Output2";
+            this.Output2.Size = new System.Drawing.Size(309, 20);
+            this.Output2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 382);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Задание2";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox y;
+        private System.Windows.Forms.TextBox Output1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox x;
+        private System.Windows.Forms.TextBox Output2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Priz;
+    }
+}
+
---
+++ WF2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            float x = float.Parse(this.x.Text); 
+            float y = float.Parse(this.y.Text);
+
+            if ((x > 10 || x < 0) || (y > 5 || y < 0))
+                this.Output1.Text = "Вне";
+            else if ((x < 10 && x > 0) && (y < 5 && y > 0))
+                this.Output1.Text = "Внутри";
+            else
+                this.Output1.Text = "На границе";
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string i = this.Priz.Text;
+            if (i == "а") this.Output2.Text = "Максимальная скорость автомобиля = 228 км/ч"; 
+            else if (i == "в") this.Output2.Text = "Максимальная скорость велосипеда = 50 км/ч";
+            else if (i == "м") this.Output2.Text = "Максимальная скорость мотоцикла = 402 км/ч";
+            else if (i == "с") this.Output2.Text = "Максимальная скорость самолета = 8200,8 км/ч";
+            else if (i == "п") this.Output2.Text = "Максимальная скорость поезда = 581 км/ч";
+            else
+                this.Output2.Text = "Такого признака нет!";
+                
+        }
+    }
+}
